338ce12e3f70fcf38802bc15e2fb2e87337af609
mhh <haywardhill@gmail.com>
2022-01-28 12:25:22 +0000

    add dev environment

---
+++ terraform/environments/dev/input.tf
@@ -0,0 +1,23 @@
+variable project_name {}
+
+variable location {}
+
+variable environment_name {
+	default = "dev"
+}
+
+variable db_username {
+  description = "Username"
+  type        = string
+  sensitive   = true
+}
+
+variable db_password {
+  description = "Password"
+  type        = string
+  sensitive   = true
+}
+
+
+
+
---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+  source               = "../../modules/resource_group"
+  resource_group       = "${var.project_name}-resources-${var.environment_name}"
+  location             = var.location
+  project              = var.project_name
+}
+
+module "log_analytics" {
+  source               = "../../modules/log_analytics"
+  location             = var.location
+  resource_group       = module.resource_group.resource_group_name
+  workspace_name       = "${var.project_name}-logs-${var.environment_name}"
+  project              = var.project_name
+}
+
+module "postgresql" {
+  source                     = "../../modules/postgresql"
+  location                   = var.location
+  resource_group             = module.resource_group.resource_group_name
+  server_name	               = "${var.project_name}-pgss-${var.environment_name}"
+  db_username                = var.db_username
+  db_password                = var.db_password
+  project                    = var.project_name
+  log_analytics_workspace_id = module.log_analytics.log_analytics_workspace_id
+  depends_on = [module.log_analytics]
+}
